f21354f24a309afc2945370475a6a75bd6bf201b
Michal <khakaimic4@gmail.com>
2023-05-20 16:04:30 +0300

    completed changes

--- control_flow.rb
+++ control_flow.rb
@@ -24,8 +24,7 @@ def fizzbuzz(num)
   # your code here
   if num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
-  elsif num % 3 == 0
-    %
+  elsif num % 3 == 0    
     "Fizz"
   elsif num % 5 == 0
     "Buzz"
